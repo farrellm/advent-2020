0cc1ef2161ddf6bfa3bd52ac437088c29900f6af
Matthew Farrell <farrellm@alum.mit.edu>
2020-12-07 00:58:06 -0500

    add template

---
+++ DayX.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TypeApplications #-}
+
+module DayX where
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import qualified Relude.Unsafe as U
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+entry :: Parser ()
+entry = pure ()
+
+part1 :: FilePath -> IO ()
+part1 f = do
+  es <- unsafeParse (entry `sepEndBy` eol) f
+  pass
+
+part2 :: FilePath -> IO ()
+part2 f = do
+  pass
+
+test :: IO ()
+test = do
+  part1 "data/testX.txt"
+  -- part1 "data/dayX.txt"
+  -- part2 "data/testX.txt"
+  -- part2 "data/dayX.txt"
+  pass
